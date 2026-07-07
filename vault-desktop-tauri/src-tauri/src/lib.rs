839622c3e38f92bb03fec722d2842d8d0a849228
mdsaimon1232 <maybenafis@gmail.com>
2026-07-07 22:37:10 +0600

    revert: rollback all updater and backend changes per user request

--- vault-desktop-tauri/src-tauri/src/lib.rs
+++ vault-desktop-tauri/src-tauri/src/lib.rs
@@ -1489,7 +1489,6 @@ async fn generate_secondary_pairing_payload(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             let _ = app.get_webview_window("main").map(|w| {
